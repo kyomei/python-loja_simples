3253a3c56dd3cce551ec1d00fb53eba47558ddd2
Rafael Jeferson <rafa.jefer@gmail.com>
2019-09-16 19:51:25 -0300

    Exibindo imagens nos produtos

--- languages/pt-br.py
+++ languages/pt-br.py
@@ -101,6 +101,7 @@
 'Errors': 'Erros',
 'export as csv file': 'exportar como um arquivo csv',
 'FAQ': 'Perguntas frequentes',
+'file': 'file',
 'First name': 'Nome',
 'Forms and Validators': 'Formulários e Validadores',
 'Free Applications': 'Aplicações gratuitas',
