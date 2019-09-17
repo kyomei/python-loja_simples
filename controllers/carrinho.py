c3bc40ff1505017ab8dbad26b9c4dfb65becf8aa
Rafael J <rafael.pena@visie.com.br>
2019-09-17 17:49:58 -0300

    Removendo itens do carrinho com session

--- controllers/carrinho.py
+++ controllers/carrinho.py
@@ -19,6 +19,22 @@ def adicionar():
     # Redireciona para controller atual no action index
     redirect(URL('index'))
 
+def remover():
+    # Remove item do carrinho
+    id = request.args[0]
+
+    # Verifica se carrinho possui item, antes de remover do carrinho
+    if len(session.carrinho):
+        # Percorre todos os itens do carrinho
+        for item in session.carrinho:
+            # Verifica se item acessado na url, possui na lista
+            if int(item.id) == int(id):
+                # Remove o item da lista
+                session.carrinho.remove(item)
+
+    redirect(URL('index'))
+
+
 def limpar():
 
     # Verifica se carrinho existe e limpa
